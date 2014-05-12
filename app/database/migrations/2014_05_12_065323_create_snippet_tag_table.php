a1a0dfa008f2b8eaa61b3b4ed57beac31983ac83
masamori <r.masamori@gmail.com>
2014-05-12 15:59:06 +0900

    Add migrate

---
+++ app/database/migrations/2014_05_12_065323_create_snippet_tag_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSnippetTagTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+      Schema::create('snippet_tag', function($table)
+      {
+           $table->increments('id')->unsigned();
+           $table->integer('snippet_id')->unsigned();
+           $table->integer('tag_id')->unsigned();
+           $table->timestamps();
+      });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+  	  Schema::drop('snippet_tag');
+	}
+
+}
