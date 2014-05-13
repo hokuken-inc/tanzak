e3cd3562f0a487d031163a36c0d8c1453c820c9c
masamori <r.masamori@gmail.com>
2014-05-13 14:18:19 +0900

    Fix login form

--- app/views/session/login.blade.php
+++ app/views/session/login.blade.php
@@ -5,20 +5,32 @@
 @stop
 
 @section('body')
+  
+  <div class="container-fluid login-body">
+    <div class="row login-wrap">
+      <div class="col-sm-2 col-sm-offset-5">
+      
+        {{ Form::open(array('class'=>'form')) }}
+            @if ($error = $errors->first('password'))
+            <div class="alert alert-danger">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+              {{ $error }}
+            </div>
+            @endif
+            
+            <div class="form-group">
+              {{ Form::text('email', Input::old('email', ''), array('placeholder'=>'メールアドレス or ID', 'class' => 'form-control')) }}
+            </div>
+            <div class="form-group">
+              {{ Form::password('password', array('placeholder' => 'パスワード', 'class' => 'form-control')) }}
+            </div>
+            <div class="form-group">
+              {{ Form::submit('ログイン', array('class' => 'btn btn-danger')) }}
+            </div>
+        {{ Form::close() }}
 
-    {{ Form::open() }}
-        @if ($error = $errors->first('password'))
-        <div style="color:red;">
-            {{ $error }}
-        </div>
-        @endif
-        {{ Form::label('email', 'Eメールアドレス：') }}
-        {{ Form::text('email', Input::old('email', '')) }}
-        <br>
-        {{ Form::label('password', 'パスワード：') }}
-        {{ Form::password('password') }}
-        <br>
-        {{ Form::submit('ログイン'); }}
-    {{ Form::close() }}
+      </div>
+    </div>
+  </div>
 
 @stop
