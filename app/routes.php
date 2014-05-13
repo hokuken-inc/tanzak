7dd14db3ebea8b3253e75d3651d14218a14ed244
masamori <r.masamori@gmail.com>
2014-05-13 14:11:19 +0900

    Fix admin routing with login

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,14 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::filter('auth', function()
+{
+    if ( ! Auth::check())
+    {
+        return Redirect::to('login');
+    }
+});
+
 
 Route::any('login', array('uses' => 'SessionController@login', 'as' => 'login'));
 Route::get('/logout', array('uses' => 'SessionController@logout', 'as' => '/logout'));
@@ -17,9 +25,9 @@ Route::get('/logout', array('uses' => 'SessionController@logout', 'as' => '/logo
 Route::get('/', 'SnippetController@show');
 Route::post('/', 'SnippetController@show');
 
-Route::group(array('prefix' => 'admin'), function()
+
+Route::group(array('prefix' => 'admin', 'before'=>'auth'), function()
 {
- 
     Route::get('create',  'SnippetController@create');
 
     Route::get('edit/{id?}',  'SnippetController@edit');
